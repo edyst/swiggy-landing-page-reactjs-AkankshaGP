e1506ef5c02faed655a86b3db2328f70182fb7a2
AkankshaGP <94476132+AkankshaGP@users.noreply.github.com>
2022-03-15 13:16:54 +0530

    Adding footer headings

---
+++ footer.js
@@ -0,0 +1,176 @@
+import React from 'react'
+import "./footer.css";
+const footer = () => {
+  return (
+
+<footer>
+    <div className='footercontainer'> 
+      <div className='secCompany'>
+        <h2 className='footerh2'>COMPANY</h2>
+        <ul>
+          <li>< a href="#"> About us</a></li>
+          <li>< a href="#"> Team</a></li>
+          <li>< a href="#"> Careers</a></li>
+          <li>< a href="#"> SwiggyBlog</a></li>
+          <li>< a href="#"> Bug Bounty</a></li>
+          <li>< a href="#"> Swiggy Super</a></li>
+          <li>< a href="#"> Swiggy Cororates</a></li>
+          <li>< a href="#"> Swiggy Instamart</a></li>
+        </ul>
+      </div>
+      <div className='secContact'>
+        <h2 className='footerh2'>CONTACT</h2>
+        <ul>
+          <li>< a href="#"> Help & Support</a></li>
+          <li>< a href="#"> Partner with us</a></li>
+          <li>< a href="#"> Ride with us</a></li>
+        </ul>
+      </div>
+      <div className='seclegal'>
+        <h2 className='footerh2'>LEGAL</h2>
+        <ul>
+          <li>< a href="#"> Terms & Conditions</a></li>
+          <li>< a href="#"> Refund & Cancellation</a></li>
+          <li>< a href="#"> Privacy Policy</a></li>
+          <li>< a href="#"> Cookie Policy</a></li>
+          <li>< a href="#"> Offer Terms</a></li>
+          <li>< a href="#"> Phishing & Fraud</a></li>
+        </ul>
+      </div>
+      <div className='secimglogo'>
+        <ul>
+        < a href="#">  <img src="https://web.archive.org/web/20210903174711im_/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-AppStore_lg30tv" /> </a>
+        < a href="#">  <img src="https://web.archive.org/web/20210903174711im_/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-GooglePlay_1_zixjxl" /></a>
+        </ul>
+      </div>
+     </div>
+      
+
+      <div className="footer">
+      <h2 className='headerf'>WE DELIVER TO</h2>
+        <div className='containerf'>
+            <div className='footerbox'>
+              <ul>
+                <li><a href='#'>Abohar</a></li>
+                <li><a href='#'>Adilabad</a></li>
+                <li><a href='#'>Adityapur</a></li>
+                <li><a href='#'>Adoni</a></li>
+                <li><a href='#'>Agartala</a></li>
+                <li><a href='#'>Agra</a></li>
+                <li><a href='#'>Ahmedabad</a></li>
+                <li> <a href='#'>Amritsar</a></li>
+                <li><a href='#'> Amroha</a></li>
+                <li><a href='#'> Anand</a></li>
+                <li><a href='#'> Anantapur</a></li>
+                <li><a href='#'> Ankleshwar</a></li>
+                <li> <a href='#'> Arrah</a></li>
+                <li><a href='#'>Asansol</a></li>
+               </ul>
+            </div>
+
+            <div className='footerbox'>
+
+              <ul>
+              <li><a href='#'> Baddi</a></li>
+                <li><a href='#'> Bagalkot</a></li>
+                <li><a href='#'>Bagdogra</a></li>
+                <li><a href='#'>Bahadurgarh</a></li>
+                <li><a href='#'>Bahraich</a></li>
+                <li><a href='#'> Bajpe</a></li>
+                <li><a href='#'>Balaghat</a></li>
+                <li><a href='#'> Ballari</a></li>
+                <li> <a href='#'>Balrampur</a></li>
+                <li> <a href='#'>Balurghat</a></li>
+                <li> <a href='#'>Ballari</a></li>
+                <li> <a href='#'>Balrampur</a></li>
+                <li><a href='#'>Balurghat</a></li>
+                <li><a href='#'>Bangalore</a></li>
+               
+              </ul>
+            </div>
+
+
+            <div className='footerbox'>
+              <ul>
+              <li> <a href='#'>Basirhat</a></li>
+                <li> <a href='#'>Basti</a></li>
+                <li><a href='#'>Batala</a></li>
+                <li><a href='#'>Bathinda</a></li>
+                <li><a href='#'>Beawar</a></li>
+                <li> <a href='#'>Beed</a></li>
+                <li><a href='#'> Begusarai</a></li>
+                <li><a href='#'>Belgaum</a></li>
+                <li> <a href='#'>Berhampore</a></li>
+                <li><a href='#'> Berhampur</a></li>
+                <li><a href='#'>Bettiah</a></li>
+                <li><a href='#'>Betul</a></li>
+                <li><a href='#'>Bhadrachalam</a></li>
+                <li><a href='#'>Bhadravati</a></li>
+               
+              </ul>
+            </div>
+
+
+            <div className='footerbox'>
+              <ul>
+                
+              <li><a href='#'>Bhubaneswar</a></li>
+                <li><a href='#'> Bhuj</a></li>
+                <li><a href='#'>Bidar</a></li>
+                <li><a href='#'>Biharsharif</a></li>
+                <li><a href='#'>Bijapur</a></li>
+                <li><a href='#'>Bijnor</a></li>
+                <li><a href='#'>Bikaner</a></li>
+                <li> <a href='#'> Bilaspur</a></li>
+                <li><a href='#'>Bina</a></li>
+                <li><a href='#'>Bodhan-Rural</a></li>
+                <li><a href='#'>Bokaro</a></li>
+                <li><a href='#'>Bolpur</a></li>
+                <li><a href='#'>Bongaigaon</a></li>
+                <li><a href='#'>Bongaon</a></li>
+              </ul>
+              </div>
+            </div>
+          </div>
+        
+      
+
+
+<div className='footerbottom'>
+    <img className='bottomi' src="https://web.archive.org/web/20210903174711im_/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_284/Logo_f5xzza" height="50px" width="150px" alt=''/>
+    <p>&copy;2021 Swiggy</p>
+    <div className="footerimg">
+      <img className="imgf1"src="https://web.archive.org/web/20210903174711im_/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-facebook_tfqsuc" height="24" width="24" alt=''/>
+      <img className="imgf2" src="https://web.archive.org/web/20210903174711im_/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-pinterest_kmz2wd" height="24" width="24" alt=''/>
+      <img className="imgf3"src="https://web.archive.org/web/20210903174711im_/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-instagram_b7nubh" height="24" width="24" alt=''/>
+      <img className="imgf4"src="https://web.archive.org/web/20210903174711im_/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-twitter_gtq8dv" height="24" width="24" alt=''/>
+    </div>
+    </div>
+
+
+
+</footer>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+     
+      
+
+
+
+  )
+}
+
+export default footer
